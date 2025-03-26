d757213f646c79a24b936e71e948997cf4063b4f
RuneRogue <priyankmakwana305@gmail.com>
2025-03-26 22:33:20 +0530

    first commit

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+    FundMe public fundMe;
+    function run() public returns (FundMe){
+        HelperConfig helperConfig = new HelperConfig();
+        address priceFeed = helperConfig.activeNetworkConfig();
+        vm.startBroadcast();
+        fundMe = new FundMe(priceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {MockV3Aggregator} from "../test/mocks/MockAggregatorV3.sol";
+
+contract HelperConfig is Script {
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid==11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        }
+        else if (block.chainid==1) {
+            activeNetworkConfig = getEthMainnetConfig();
+        }
+        else {
+            activeNetworkConfig = getAnvilConfig();
+        }
+    }
+
+    function getSepoliaConfig() public returns (NetworkConfig memory) {
+        activeNetworkConfig = NetworkConfig(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return activeNetworkConfig;
+    }
+
+    function getEthMainnetConfig() public returns (NetworkConfig memory) {
+        activeNetworkConfig = NetworkConfig(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        return activeNetworkConfig;
+    }
+
+    function getAnvilConfig() public returns (NetworkConfig memory){
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockAggregator = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+        activeNetworkConfig = NetworkConfig(address(mockAggregator));
+        return activeNetworkConfig;
+    }
+}
